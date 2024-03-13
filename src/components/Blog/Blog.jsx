c84b72067e132cb6ba434ab97530aa0c725e8574
mozzammelrobi <mozzammelfb@gmail.com>
2024-03-13 13:25:21 +0600

    displayed data dynamicaly on blogs part

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,36 @@
+import { space } from 'postcss/lib/list';
 import PropTypes from 'prop-types'; 
 
 const Blog = ({blog}) => {
-    console.log(blog)
+    
+    const {title, cover,author,author_img,posted_date,hashtags,reading_time}= blog
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-full' src={cover} alt="" />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14 rounded-full' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} minute</span>
+                </div>
+            </div>
+            <h3 className="text-4xl">{title}</h3>
+            <p className='flex gap-6'>
+                {
+                    hashtags.map((has, idx) => <span key={idx}><a href="">{has}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
 
 Blog.propTypes = {
-    blog:PropeTypes.object
+    blog: PropTypes.object,
 }
 export default Blog;
