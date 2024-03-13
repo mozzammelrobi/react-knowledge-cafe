220595d545701950063cb80599e2e4b05068fffb
mozzammelrobi <mozzammelfb@gmail.com>
2024-03-13 14:10:11 +0600

    handle bookmark state declare and display bookmark

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
+  const [bookmarks, setBookmarks]= useState([])
+
+  const handleAddBookmard = blog =>{
+    console.log('add book mark')
+  }
 
   return (
     <>
       <Header></Header>
       <div className='md:flex max-w-7xl mx-auto'>
-        <Blogs></Blogs>
+        <Blogs 
+          handleAddBookmard = {handleAddBookmard}
+        ></Blogs>
         <Bookmarks></Bookmarks>
       </div>
     </>
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,13 +1,14 @@
 import { space } from 'postcss/lib/list';
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
+import { IoIosBookmark } from "react-icons/io"; 
 
-const Blog = ({blog}) => {
+const Blog = ({blog, handleAddBookmard}) => {
     
     const {title, cover,author,author_img,posted_date,hashtags,reading_time}= blog
     return (
         <div className='mb-20'>
-            <img className='w-full' src={cover} alt="" />
-            <div className='flex justify-between'>
+            <img className='w-full mb-8' src={cover} alt="" />
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
                     <img className='w-14 rounded-full' src={author_img} alt="" />
                     <div className='ml-6'>
@@ -15,11 +16,15 @@ const Blog = ({blog}) => {
                         <p>{posted_date}</p>
                     </div>
                 </div>
-                <div>
-                    <span>{reading_time} minute</span>
+                <div className='flex flex-row justify-center items-center'>
+                    <span>{reading_time} min read</span>
+                    <button 
+                        onClick={handleAddBookmard}
+                        className='ml-3 text-2xl'
+                    ><IoIosBookmark /></button>
                 </div>
             </div>
-            <h3 className="text-4xl">{title}</h3>
+            <h3 className="text-4xl mb-4">{title}</h3>
             <p className='flex gap-6'>
                 {
                     hashtags.map((has, idx) => <span key={idx}><a href="">{has}</a></span>)
